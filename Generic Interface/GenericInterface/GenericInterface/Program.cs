53f2a97ca72a034d1a962791e10ac1937baa730c
Shivam Soni <sonishivam1402@gmail.com>
2024-08-21 23:23:17 +0530

    Generic As Argument and Return type added

---
+++ Generic Interface/GenericInterface/GenericInterface/Program.cs
@@ -0,0 +1,47 @@
+﻿// Generic Interface
+public interface IComparer<T>
+{
+    int Compare(T x, T y);
+}
+
+// Class implementing the generic interface for comparing two Person objects
+public class PersonComparer : IComparer<Person>
+{
+    public int Compare(Person x, Person y)
+    {
+        return x.Age.CompareTo(y.Age); // Compare by Age
+    }
+}
+
+// Simple Person class
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Person person1 = new Person { Name = "Shivam", Age = 22 };
+        Person person2 = new Person { Name = "Jiya", Age = 50 };
+
+        PersonComparer comparer = new PersonComparer();
+        int result = comparer.Compare(person1, person2);
+
+        if (result < 0)
+        {
+            Console.WriteLine($"{person1.Name} is younger than {person2.Name}");
+        }
+        else if (result > 0)
+        {
+            Console.WriteLine($"{person1.Name} is older than {person2.Name}");
+        }
+        else
+        {
+            Console.WriteLine($"{person1.Name} and {person2.Name} are the same age");
+        }
+    }
+}
---
+++ Generic as Argument and Return Type/ArgumentAndReturnType/ArgumentAndReturnType/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        // Swapping integers
+        var swappedInts = Swap(1, 2);
+        Console.WriteLine($"Swapped: {swappedInts.Item1}, {swappedInts.Item2}"); // Outputs: Swapped: 2, 1
+
+        // Swapping strings
+        var swappedStrings = Swap("Hello", "World");
+        Console.WriteLine($"Swapped: {swappedStrings.Item1}, {swappedStrings.Item2}"); // Outputs: Swapped: World, Hello
+    }
+
+    // Generic method
+    public static (T, T) Swap<T>(T first, T second)
+    {
+        return (second, first);
+    }
+}
